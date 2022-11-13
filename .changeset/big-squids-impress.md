2b00f78ca152a055ace8181e57d1364d60b81656
Cristi Petre <driimus@vivaldi.net>
2022-11-13 18:29:59 +0000

    test(permanent-failure): send to dlq

---
+++ .changeset/big-squids-impress.md
@@ -0,0 +1,5 @@
+---
+'permanent-failure-dlq': patch
+---
+
+fix: transfer failures to dlq in batches of 10
--- packages/permanent-failure-dlq/src/index.test.ts
+++ packages/permanent-failure-dlq/src/index.test.ts
@@ -1,3 +1,105 @@
+import 'aws-sdk-client-mock-jest';
+
+import { SendMessageBatchCommand, SQSClient } from '@aws-sdk/client-sqs';
+import { faker } from '@faker-js/faker';
+import { mockClient } from 'aws-sdk-client-mock';
+import { SQSBatchProcessor } from 'base-batch-processor';
+import { sqsEventFactory, sqsMessageAttributeFactory, sqsRecordFactory } from 'mock-events';
+
+import { PermanentFailureDLQHandler } from '.';
+
+const mockSQS = mockClient(SQSClient);
+
+beforeEach(() => {
+  mockSQS.reset();
+});
+
 describe('PermanentFailureDLQHandler', () => {
-  it.todo('should send provided records to DLQ');
+  const queueUrl = faker.internet.url();
+  const toDLQHandler = new PermanentFailureDLQHandler(queueUrl);
+
+  const failures = sqsRecordFactory
+    .buildList(2, {
+      messageAttributes: {
+        attributeOne: sqsMessageAttributeFactory.string().build(),
+        attributeTwo: sqsMessageAttributeFactory.number().build(),
+        attributeThree: sqsMessageAttributeFactory.binary().build(),
+        attributeFour: sqsMessageAttributeFactory.binary().build({ binaryValue: undefined }),
+      },
+    })
+    .map((record) => {
+      return { record, reason: faker.lorem.sentence() };
+    });
+
+  it('should send provided records to the configured queue', async () => {
+    mockSQS.resolves({});
+
+    await expect(toDLQHandler.handleRejections(failures)).resolves.toBeUndefined();
+    expect(mockSQS).toHaveReceivedCommandTimes(SendMessageBatchCommand, 1);
+    expect(mockSQS).toHaveReceivedCommandWith(SendMessageBatchCommand, {
+      QueueUrl: queueUrl,
+      Entries: expect.objectContaining({ length: failures.length }),
+    });
+  });
+
+  it('should surface rejections', async () => {
+    mockSQS.rejectsOnce({});
+
+    await expect(toDLQHandler.handleRejections(failures)).rejects.toThrow();
+    expect(mockSQS).toHaveReceivedCommandTimes(SendMessageBatchCommand, 1);
+  });
+
+  it('should not publish for empty failure lists', async () => {
+    mockSQS.rejectsOnce({});
+
+    await expect(toDLQHandler.handleRejections([])).resolves.toBeUndefined();
+    expect(mockSQS).not.toHaveReceivedCommand(SendMessageBatchCommand);
+  });
+
+  it('should move messages in batches of 10', async () => {
+    mockSQS.resolves({});
+
+    const failures = sqsRecordFactory.buildList(12).map((record) => {
+      return { record, reason: faker.lorem.sentence() };
+    });
+
+    await expect(toDLQHandler.handleRejections(failures)).resolves.toBeUndefined();
+    expect(mockSQS).toHaveReceivedCommandTimes(
+      SendMessageBatchCommand,
+      Math.ceil(failures.length / 10)
+    );
+  });
+
+  it('should be injectable into batch processors', async () => {
+    mockSQS.resolves({});
+    const failureHandlerSpy = jest.spyOn(toDLQHandler, 'handleRejections');
+
+    class ValidationError extends Error {
+      message = 'Validation error';
+    }
+
+    const processor = new SQSBatchProcessor({
+      // eslint-disable-next-line @typescript-eslint/require-await
+      handler: async (record) => {
+        if (record.body === 'invalid body') throw new ValidationError();
+      },
+      nonRetryableErrorHandler: toDLQHandler,
+      nonRetryableErrors: [ValidationError],
+    });
+
+    const invalidRecord = sqsRecordFactory.build({ body: 'invalid body' });
+
+    await expect(
+      processor.process(
+        sqsEventFactory.build({ Records: [invalidRecord, ...sqsRecordFactory.buildList(2)] })
+      )
+    ).resolves.toStrictEqual({
+      batchItemFailures: [],
+    });
+    expect(failureHandlerSpy).toHaveBeenCalledTimes(1);
+    expect(failureHandlerSpy).toHaveBeenCalledWith(
+      [expect.objectContaining({ record: invalidRecord, reason: expect.any(ValidationError) })],
+      undefined
+    );
+  });
 });
