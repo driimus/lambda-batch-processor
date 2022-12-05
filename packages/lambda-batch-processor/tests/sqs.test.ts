2bd2092bcb94bd52c2722d2ed99a902a03ec3e5f
Cristian <driimus@vivaldi.net>
2022-12-05 03:23:03 +0000

    test(batch-processor): collect failure identifiers

---
+++ packages/lambda-batch-processor/tests/sqs.test.ts
@@ -0,0 +1,24 @@
+import { sqsRecordFactory } from 'mock-events';
+import { describe, expect, it } from 'vitest';
+
+import { SQSBatchProcessor } from '../src/index.js';
+
+describe('SQSBatchProcessor', () => {
+  const processor = new SQSBatchProcessor(async (record) => {
+    if (record.body === 'retryable body') throw new Error('something went wrong');
+    await Promise.resolve();
+  });
+
+  it('collects failed sqs record identifiers', async () => {
+    const failingRecords = sqsRecordFactory.buildList(1, { body: 'retryable body' });
+    const Records = [...failingRecords, ...sqsRecordFactory.buildList(2)];
+
+    await expect(
+      processor.process({
+        Records,
+      })
+    ).resolves.toStrictEqual({
+      batchItemFailures: failingRecords.map(({ messageId }) => ({ itemIdentifier: messageId })),
+    });
+  });
+});
