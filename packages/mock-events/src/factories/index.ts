da49b8610b79fc7299c702b13d832511cc3f3f19
Cristian <driimus@vivaldi.net>
2022-12-10 12:52:21 +0000

    refactor(mock-events): computed events

--- packages/mock-events/src/factories/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './dynamodb.js';
-export * from './kinesis.js';
-export * from './sqs.js';
--- packages/mock-events/src/sqs.ts
+++ packages/mock-events/src/sqs.ts
@@ -1,19 +1,30 @@
-import { randomBytes } from 'node:crypto';
+import { randomBytes, randomUUID } from 'node:crypto';
 
 import { faker } from '@faker-js/faker';
 import type { SQSEvent, SQSMessageAttribute, SQSRecord } from 'aws-lambda';
 import { Factory } from 'fishery';
 
-import Event from '../events/sqs.json' assert { type: 'json' };
-
 export const sqsRecordFactory = Factory.define<SQSRecord>(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return structuredClone(Event.Records[0]);
+  return {
+    messageId: randomUUID(),
+    receiptHandle: 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
+    body: 'Test message.',
+    attributes: {
+      ApproximateReceiveCount: '1',
+      SentTimestamp: '1545082649183',
+      SenderId: 'AIDAIENQZJOLO23YVJ4VO',
+      ApproximateFirstReceiveTimestamp: '1545082649185',
+    },
+    messageAttributes: {},
+    md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
+    eventSource: 'aws:sqs',
+    eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
+    awsRegion: 'us-east-2',
+  };
 });
 
 export const sqsEventFactory = Factory.define<SQSEvent>(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return structuredClone({ Records: Event.Records });
+  return { Records: sqsRecordFactory.buildList(1) };
 });
 
 class SQSMessageAttributeFactory extends Factory<SQSMessageAttribute> {
