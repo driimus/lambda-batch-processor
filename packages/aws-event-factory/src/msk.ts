5dad2a8534186f1ec8b357f47cebe4b8de600f67
Cristian <driimus@vivaldi.net>
2024-01-22 23:08:25 +0000

    fix(event-factory): include `bootstrapServers` for msk

--- packages/aws-event-factory/src/msk.ts
+++ packages/aws-event-factory/src/msk.ts
@@ -25,12 +25,14 @@ export const mskEventFactory = Factory.define<MSKEvent, { topic: string; partiti
       eventSource: 'aws:kafka',
       eventSourceArn:
         'arn:aws:kafka:sa-east-1:123456789012:cluster/vpc-2priv-2pub/751d2973-a626-431c-9d4e-d7975eb44dd7-2',
+      bootstrapServers:
+        'b-2.demo-cluster-1.a1bcde.c1.kafka.us-east-1.amazonaws.com:9092,b-1.demo-cluster-1.a1bcde.c1.kafka.us-east-1.amazonaws.com:9092',
       records: Object.fromEntries(
         Array.from({ length: partitions }, (_v, partition) => [
           `${topic}-${partition}`,
           mskRecordFactory.buildList(1, { topic, partition }),
-        ])
+        ]),
       ),
     };
-  }
+  },
 );
